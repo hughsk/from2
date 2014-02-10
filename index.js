142ac78c53d5e32f4c96bade7fcc10b9cb4d498e
Hugh Kennedy <hughskennedy@gmail.com>
2014-02-10 21:02:37 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var Readable = require('readable-stream').Readable
+var inherits = require('inherits')
+
+module.exports = from2
+
+from2.ctor = ctor
+from2.obj = obj
+
+function from2(opts, read) {
+  return new (ctor(opts, read))
+}
+
+function ctor(opts, read) {
+  if (typeof opts === 'function') {
+    read = opts
+    opts = {}
+  }
+
+  opts = defaults(opts)
+
+  inherits(Class, Readable)
+  function Class() {
+    if (!(this instanceof Class)) return new Class
+    Readable.call(this, opts)
+  }
+
+  Class.prototype._from = read
+  Class.prototype._read = function(size) {
+    var self = this
+
+    this._from(size, check)
+    function check(err, data) {
+      if (err) return self.emit('error', err)
+      if (data === null) return self.push(null)
+      if (self.push(data)) self._from(size, check)
+    }
+  }
+
+  return Class
+}
+
+function obj(opts, read) {
+  if (typeof opts === 'function') {
+    read = opts
+    opts = {}
+  }
+
+  opts = defaults(opts)
+  opts.objectMode = true
+
+  return from2(opts, read)
+}
+
+function defaults(opts) {
+  opts = opts || {}
+  return opts
+}
---
+++ test.js
@@ -0,0 +1,43 @@
+var test = require('tape')
+var path = require('path')
+var from = require('./')
+var fs   = require('fs')
+
+var tmp = path.resolve(
+  __dirname, 'tmp.txt'
+)
+
+function fromString(string) {
+  return from(function(size, next) {
+    if (string.length <= 0) return next()
+    var chunk = string.slice(0, size)
+    string = string.slice(size)
+    next(null, chunk)
+  })
+}
+
+test('from2', function(t) {
+  var contents = fs.readFileSync(__filename, 'utf8')
+  var stream = fromString(contents)
+
+  stream
+    .pipe(fs.createWriteStream(tmp))
+    .on('close', function() {
+      t.equal(fs.readFileSync(tmp, 'utf8'), contents)
+      fs.unlinkSync(tmp)
+      t.end()
+    })
+})
+
+test('old mode', function(t) {
+  var contents = fs.readFileSync(__filename, 'utf8')
+  var stream = fromString(contents)
+  var buffer = ''
+
+  stream.on('data', function(data) {
+    buffer += data
+  }).on('end', function() {
+    t.equal(buffer, contents)
+    t.end()
+  })
+})
